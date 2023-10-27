bcbee62140805d96a55b49f15de0f75744e8fd2b
Lovestrant <kemboilovestrant@gmail.com>
2023-10-27 09:52:30 +0300

    Got rid of unnecessary global CSS file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
 
-import NavBar from '../components/shared/navbar';
+import NavBar from "../components/shared/navbar";
 export default function Home() {
   return (
     <div>
       <NavBar />
-    </div>)
+    </div>
+  );
 }
