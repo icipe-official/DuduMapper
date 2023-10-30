6ab125f2c3226178e774f04aa2caad213280ae2c
gituman <petergituu@gmail.com>
2023-10-30 11:24:39 +0300

    added map

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,12 @@ import Image from 'next/image'
 import styles from './page.module.css'
 
 import NavBar from '../components/shared/navbar';
+
+
 export default function Home() {
   return (
     <div>
       <NavBar />
-    </div>)
+    </div>
+  )
 }
