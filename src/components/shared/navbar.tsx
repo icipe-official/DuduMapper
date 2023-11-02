9ee7e6053a51c35178b2f141fc615142c3ccaab0
Mandela Mitau <digitalcreative@mandelamitau.com>
2023-11-02 15:48:29 +0300

    edit navbar styling

--- src/components/shared/navbar.tsx
+++ src/components/shared/navbar.tsx
@@ -1,19 +1,18 @@
 
 import React from 'react';
-import { AppBar, Toolbar, Button, IconButton } from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
+import { AppBar, Toolbar, Button } from '@mui/material';
 
 const Navbar: React.FC = () => {
   return (
-    <AppBar position="static">
+    <AppBar position="static" style={{ backgroundColor: 'white', boxShadow: '1px 1px 1px 1px gray' }}>
       <Toolbar>
         <img src={"/vector-atlas-logo.svg"} alt="Logo" style={{ height: '40px', marginRight: '16px' }} />
         <div style={{ flexGrow: 1 }}></div>
-
-        <Button color="inherit">Map</Button>
+        <Button color="primary">Map</Button>
       </Toolbar>
     </AppBar>
   );
 };
 
 export default Navbar;
+
