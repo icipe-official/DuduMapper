805461c3aa3a1be63baf3faae6f1fc91b6478756
Mandela Mitau <digitalcreative@mandelamitau.com>
2023-11-10 12:28:22 +0300

    add accordion to display occurrence information

--- src/components/shared/map.tsx
+++ src/components/shared/map.tsx
@@ -21,22 +21,33 @@ import { bbox as bboxStrategy } from "ol/loadingstrategy.js";
 import { Tile as TileLayer, Vector as VectorLayer } from "ol/layer.js";
 import { geoServerBaseUrl } from "@/requests/requests";
 import { Pixel } from "ol/pixel";
-
 import MapBrowserEvent from 'ol/MapBrowserEvent';
 import Event from 'ol/events/Event';
 import Popover from '@mui/material/Popover';
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 
 
 function Newmap() {
   const [anchorEl, setAnchorEl] = React.useState<HTMLElement | null>(null);
-  const [popoverContent, setPopoverContent] = React.useState<string>('');
+  const [popoverContent, setPopoverContent] = React.useState<{ [x: string]: any }>({});
   const open = Boolean(anchorEl);
   const id = open ? 'feature-popover' : undefined;
   console.log("geoServerBaseUrl: " + geoServerBaseUrl);
   const [map, setMap] = useState<Map | undefined>(); // Specify the type using a generic type argument
   const mapElement = useRef<HTMLDivElement>(null);
   const mapRef = useRef<Map | undefined>(undefined);
+  const [expanded, setExpanded] = React.useState<string | false>(false);
+
+  const handleChange =
+    (panel: string) => (event: React.SyntheticEvent, isExpanded: boolean) => {
+      setExpanded(isExpanded ? panel : false);
+    };
+
   const handleClosePopover = () => {
     if (anchorEl) {
       anchorEl.remove(); // This removes the dummy anchor from the DOM
@@ -334,7 +345,7 @@ function Newmap() {
           }
 
           // Set the state for Popover content and anchor
-          setPopoverContent(JSON.stringify(feature.getProperties(), null, 2));
+          setPopoverContent(feature.getProperties());
           setAnchorEl(dummyAnchor);
 
           // Return true to stop the forEach loop if needed
@@ -372,7 +383,90 @@ function Newmap() {
         onClose={handleClosePopover}
       // ... other props
       >
-        {"hello"}
+
+        <div>
+          <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1bh-content"
+              id="panel1bh-header"
+            >
+              <Typography sx={{ width: '33%', flexShrink: 0 }}>
+                Species
+              </Typography>
+              <Typography sx={{ color: 'text.secondary' }}>Mosquito species details</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                {popoverContent?.notes}
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel2bh-content"
+              id="panel2bh-header"
+            >
+              <Typography sx={{ width: '33%', flexShrink: 0 }}>Bionomics</Typography>
+              <Typography sx={{ color: 'text.secondary' }}>
+                Ecological characteristics of species
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                {popoverContent?.notes}
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel3bh-content"
+              id="panel3bh-header"
+            >
+              <Typography sx={{ width: '33%', flexShrink: 0 }}>
+                Site/Environment
+              </Typography>
+              <Typography sx={{ color: 'text.secondary' }}>
+                Geographical and environmental data
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                {popoverContent.geometry ? (
+                  <>
+                    Location: {popoverContent.geometry.layout}
+                    Coordinates: {popoverContent.geometry.flatCoordinates.join(', ')}
+                  </>
+                ) : (
+                  <span>Loading or no data available...</span>
+                )}
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel4bh-content"
+              id="panel4bh-header"
+            >
+              <Typography sx={{ width: '33%', flexShrink: 0 }}>
+                Period
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                {popoverContent?.start_month && popoverContent?.start_year && popoverContent?.end_month && popoverContent?.end_year ? (
+                  `Data collected from ${popoverContent.start_month}/${popoverContent.start_year} to ${popoverContent.end_month}/${popoverContent.end_year}.`
+                ) : (
+                  <span>Loading or no data available...</span>
+                )}
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+        </div>
+
       </Popover>
 
 
