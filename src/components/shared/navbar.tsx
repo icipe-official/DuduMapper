5045140960a89408c6ff9adce0696e7285bc4feb
shishi5089 <shishinangs83@gmail.com>
2023-11-21 14:31:19 +0300

    added changes to styling

--- src/components/shared/navbar.tsx
+++ src/components/shared/navbar.tsx
@@ -21,7 +21,7 @@ const Navbar: React.FC = () => {
   return (
     <div>
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="fixed" sx={{ bgcolor: 'white', margin: '0' }}>
+      <AppBar position="fixed" sx={{ bgcolor: 'white', margin: '0', padding: '0'}}>
         <Toolbar>
           <>
             <Box sx={{ flexGrow: 1, mt: '6px' }}>
@@ -29,7 +29,7 @@ const Navbar: React.FC = () => {
                 <picture>
                   <img
                     src="/vector-atlas-logo.svg"
-                    style={{ maxHeight: '60px', cursor: 'pointer' }}
+                    style={{ maxHeight: '50px', cursor: 'pointer' }}
                     alt="Vector Atlas logo"
                   />
                 </picture>
