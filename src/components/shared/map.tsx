4a13963aca6c99ac66077e051b589ee7a628dfb2
Mandela Mitau <digitalcreative@mandelamitau.com>
2023-11-21 11:41:11 +0300

    remove the occurrence pop up from map component to its own component

--- src/components/shared/map.tsx
+++ src/components/shared/map.tsx
@@ -26,6 +26,7 @@ import {
 } from "@/requests/requests";
 import "./CSS/LayerSwitcherStyles.css";
 import { Stroke, Fill, Style, Circle } from "ol/style";
+import OccurrencePopup from "../map/occurrence_popup";
 function Newmap() {
   const [anchorEl, setAnchorEl] = React.useState<HTMLElement | null>(null);
   const [popoverContent, setPopoverContent] = React.useState<{ [x: string]: any }>({});
@@ -174,99 +175,15 @@ function Newmap() {
       >
       </div>
 
-
-
-      <Popover
+      <OccurrencePopup
         id={id}
         open={open}
         anchorEl={anchorEl}
-        onClose={handleClosePopover}
-      // ... other props
-      >
-
-        <div>
-          <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
-            <AccordionSummary
-              expandIcon={<ExpandMoreIcon />}
-              aria-controls="panel1bh-content"
-              id="panel1bh-header"
-            >
-              <Typography sx={{ width: '33%', flexShrink: 0 }}>
-                Species
-              </Typography>
-              <Typography sx={{ color: 'text.secondary' }}>Mosquito species details</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography>
-                {popoverContent?.notes}
-              </Typography>
-            </AccordionDetails>
-          </Accordion>
-          <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
-            <AccordionSummary
-              expandIcon={<ExpandMoreIcon />}
-              aria-controls="panel2bh-content"
-              id="panel2bh-header"
-            >
-              <Typography sx={{ width: '33%', flexShrink: 0 }}>Bionomics</Typography>
-              <Typography sx={{ color: 'text.secondary' }}>
-                Ecological characteristics of species
-              </Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography>
-                {popoverContent?.notes}
-              </Typography>
-            </AccordionDetails>
-          </Accordion>
-          <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
-            <AccordionSummary
-              expandIcon={<ExpandMoreIcon />}
-              aria-controls="panel3bh-content"
-              id="panel3bh-header"
-            >
-              <Typography sx={{ width: '33%', flexShrink: 0 }}>
-                Site/Environment
-              </Typography>
-              <Typography sx={{ color: 'text.secondary' }}>
-                Geographical and environmental data
-              </Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography>
-                {popoverContent.geometry ? (
-                  <>
-                    Location: {popoverContent.geometry.layout}
-                    Coordinates: {popoverContent.geometry.flatCoordinates.join(', ')}
-                  </>
-                ) : (
-                  <span>Loading or no data available...</span>
-                )}
-              </Typography>
-            </AccordionDetails>
-          </Accordion>
-          <Accordion expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>
-            <AccordionSummary
-              expandIcon={<ExpandMoreIcon />}
-              aria-controls="panel4bh-content"
-              id="panel4bh-header"
-            >
-              <Typography sx={{ width: '33%', flexShrink: 0 }}>
-                Period
-              </Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography>
-                {popoverContent?.period_start && popoverContent?.period_end ? (
-                  `Data collected from ${popoverContent.period_start} to ${popoverContent.period_end}.`
-                ) : (
-                  <span>Loading or no data available...</span>
-                )}
-              </Typography>
-            </AccordionDetails>
-          </Accordion>
-        </div>
-      </Popover>
+        handleClose={handleClosePopover}
+        popoverContent={popoverContent}
+        expanded={expanded}
+        handleChange={handleChange}
+      />
     </>
   );
 }
