755ef37c10b5a91873ac49ccb4b4b949d54e0161
mikejack876 <mikebossjack>
2025-04-24 13:47:31 +0300

    Revert "Merge branch 'feature/loader-left' into feature/sidepanel"
    This reverts commit b792987e0e939a11e7f8c87f16091b2ee129f352, reversing
    changes made to 4d80ec0ef4ea790b2c82ce677eb12d5ad1d046ec.

--- src/components/shared/navbar.tsx
+++ src/components/shared/navbar.tsx
@@ -8,33 +8,10 @@ import Box from "@mui/material/Box";
 import NavLink from "./navlink";
 import { useMediaQuery, useTheme } from "@mui/material";
 import { BASE_PATH } from "@/lib/constants";
-//import { useNavigate } from "react-router-dom";
-//import { Link } from "react-router-dom";
 const Navbar: React.FC = () => {
   const theme = useTheme();
-  //const navigate = useNavigate();
-  const navigateTo = (path: string) => {
-    window.location.href = path;
-  };
   const navMenuItems: any[] = [];
-
-  //nav items calling//
-  navMenuItems.push(
-    <NavLink
-      key="About"
-      url="./about"
-      text="About" //trying onclick for handle window shift request
-      //onClick={() => navigateTo("/About")}
-    />
-  );
-  navMenuItems.push(
-    <NavLink
-      key="Register"
-      url="./auth/register"
-      text="Register" //trying onclick for handle window shift request
-      //onClick={() => navigateTo("/Register")}
-    />
-  );
+  navMenuItems.push(<NavLink key="About" url="/about" text="About" />);
 
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
 
@@ -45,8 +22,6 @@ const Navbar: React.FC = () => {
           position="fixed"
           sx={{ bgcolor: "white", margin: "0", padding: "0" }}
         >
-          {/* about link click function*/}
-
           <Toolbar>
             <>
               <Box sx={{ flexGrow: 1, mt: "6px" }}>
