2b8d050b6bac9a7f48082fbd7fddc0c6509b0788
mikejack857 <mikebossjack@gmail.com>
2025-08-19 14:19:00 +0300

    hardord idw 2024

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -240,12 +240,13 @@ function Newmap() {
     if (yearMatch) return yearMatch[1];
 
     // fallback logic for IDW models that have a month but no year
-    /* if (
+    //will remove this fallback after
+    if (
       layerName.toLowerCase().includes("idw_model") &&
       extractMonth(layerName)
     ) {
       return "2024"; // fallback assumption
-    }*/
+    }
 
     return null;
   };
